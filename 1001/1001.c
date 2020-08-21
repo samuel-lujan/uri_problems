45ba6f5cd2f8bb4fc7c6dddfe62c895881208989
Samuel Lujan <samuel_lujan@hotmail.com>
2020-08-21 19:34:21 -0300

    URI PROBLEMS BEGGINNER Extremely Basic

---
+++ 1001/1001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int A, B;
+    int X;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    X = A+B;
+    printf("X = %d\n", X);
+    return 0;
+}
