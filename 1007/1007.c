6ee06e48d773f42ece2dfd4263652e076972de7d
Samuel Lujan <samuel_lujan@hotmail.com>
2020-08-22 11:45:58 -0300

    URI PROBLEMS BEGGINER Difference

---
+++ 1007/1007.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int A, B, C, D;
+    scanf("%d %d %d %d", &A, &B, &C, &D);    
+    printf("DIFERENCA = %d\n", ((A*B)-(C*D)));
+    system("pause");
+    return  0;
+}
