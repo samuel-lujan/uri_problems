151d674fcad30e2749f0b03b7551de9ef3a306f5
Samuel Lujan <samuel_lujan@hotmail.com>
2020-08-23 15:31:51 -0300

    URI BEGINNER PROBLEMS Distance Between Two Points

---
+++ BEGINNER/1015/1015.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void){
+    double x1, x2, y1, y2;
+    scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+    float distancia = sqrt( pow((x2-x1),2)+pow((y2-y1),2) );    
+    printf("%.4f\n", distancia);
+    return 0;
+}
